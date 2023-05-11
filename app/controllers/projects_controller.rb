9fce19b70b8bc2b69f472975cc1a276448d2cea1
Mudassar Husnain <mudassar.husnain@nxb.com.pk>
2023-05-11 18:36:30 +0500

    add some styling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,7 @@ class ProjectsController < ApplicationController
     before_action :set_project, only: %i[ show edit update destroy ]
   def index
     @projects = Project.all
+    @projects=@projects.reverse
   end
   def new
     @project = Project.new
