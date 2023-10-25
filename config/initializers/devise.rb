e5b45199782c818975386358bb98c99bc691115a
Mudassar Husnain <mudassar.husnain@nxb.com.pk>
2023-10-25 14:47:21 +0500

    write command for db setup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -32,10 +32,10 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require "devise/orm/active_record"
-  config.omniauth :google_oauth2, Rails.application.credentials[:google][:client_id],
-                  Rails.application.credentials[:google][:client_secret], { scope: "userinfo.email, youtube" }
-  config.omniauth :facebook, Rails.application.credentials[:facebook][:app_id],
-                  Rails.application.credentials[:facebook][:app_secret]
+  # config.omniauth :google_oauth2, Rails.application.credentials[:google][:client_id],
+  #                 Rails.application.credentials[:google][:client_secret], { scope: "userinfo.email, youtube" }
+  # config.omniauth :facebook, Rails.application.credentials[:facebook][:app_id],
+  #                 Rails.application.credentials[:facebook][:app_secret]
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
